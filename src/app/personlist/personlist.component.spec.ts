481a5cf1652b0ba2470e55ef7986d761aab5255e
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2023-01-08 03:00:57 +0530

    Adding test condition in personlist in spec file

--- src/app/personlist/personlist.component.spec.ts
+++ src/app/personlist/personlist.component.spec.ts
@@ -9,16 +9,16 @@ describe('PersonlistComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PersonlistComponent,AppComponent ]
+      declarations: [PersonlistComponent, AppComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(PersonlistComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create the app',()=>{
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(PersonlistComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -34,6 +34,21 @@ describe('PersonlistComponent', () => {
     let headerRow = tableRows[0];
     expect(headerRow.cells[1].innerHTML).toBe('Last Name');
 
-    
+
+  });
+
+  it('testing html second table data', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    let tableRows = fixture.nativeElement.querySelectorAll('tr');
+    // expect(compiled.querySelector('td')?.textContent).toContain('kaushikghoshgmail.com');
+
+    expect(tableRows.length).toBe(5);
+
+    let headerRow = tableRows[1];
+    expect(headerRow.cells[3].innerHTML).toBe('₹67,500.00');
+
+
   });
 });
+
+
